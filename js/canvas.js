a13516f2aaa2453cc7789ce8af7b9afd1d1b4e6d
h3isenbrg <h3isenbrg1718@gmail.com>
2022-05-01 19:09:07 +0200

    fix js images

--- js/canvas.js
+++ js/canvas.js
@@ -7,3 +7,40 @@ canvas.height = 700;
 function canvasDrawbackground() {
     ctx.drawImage(background, 0, 0, 500, 700);
 }
+
+canvas.style.position = 'absolute'
+canvas.style.margin = 'auto'
+canvas.style.top = '0'
+canvas.style.bottom = '0'
+canvas.style.left = '0'
+canvas.style.right = '0'
+
+const doodle = document.createElement('img')
+doodle.src = 'src/images/doodler.png'
+
+const platform = document.createElement('img')
+platform.src = 'src/images/platform.png'
+
+const background = document.createElement('img')
+background.src = 'src/images/doodlejump.background.png'
+
+const jetpack = document.createElement('img')
+jetpack.src = 'src/images/jetpack.png'
+
+const fireball = document.createElement('img')
+fireball.src = 'src/images/fireball.png'
+
+const springcoil = document.createElement('img')
+springcoil.src = 'src/images/springcoil.png'
+
+const heli = document.createElement('img')
+heli.src = 'src/images/heli.png'
+
+const boots = document.createElement('img')
+boots.src = 'src/images/boots.png'
+
+const gunbarrel = document.createElement('img')
+gunbarrel.src = 'src/images/gun.png'
+
+const monster = document.createElement('img')
+monster.src = 'src/images/monster.png'
